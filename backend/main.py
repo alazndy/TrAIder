0df422c325bda6f17dab312b804a8689c1ed05fb
alazndy <78882672+alazndy@users.noreply.github.com>
2026-01-10 19:36:28 +0300

    Fix all pandas_ta imports to ta

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 import pandas as pd
-import pandas_ta as ta
+import ta
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
